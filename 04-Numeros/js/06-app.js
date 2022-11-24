de775246d7c7f080f193c76af176d5cc3b713b66
Andrea Gutierrez <gutierrezandrea012@gmail.com>
2022-11-23 22:50:32 -0600

    add some coments to method of numbers

--- 04-Numeros/js/06-app.js
+++ 04-Numeros/js/06-app.js
@@ -1,5 +1,7 @@
 const num_1 = "20";
 console.log(num_1);
+//regresa el tipo de dato que es
+console.log(typeof num_1);
 //convertimos un string a un numero entero
 console.log(Number.parseInt(num_1));
 const num_2 = "20.2";
