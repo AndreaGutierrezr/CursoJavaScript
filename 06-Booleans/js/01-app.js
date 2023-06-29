dd38a858c4973bf42ebc5d75de203f2736c67e0e
Andrea Gutierrez <gutierrezandrea012@gmail.com>
2023-06-28 23:07:35 -0600

    update comments to booleans class

--- 06-Booleans/js/01-app.js
+++ 06-Booleans/js/01-app.js
@@ -7,6 +7,7 @@ console.log(boolean2);
 
 console.log(typeof boolean1);
 console.log(typeof boolean2);
+console.log(typeof boolean3);
 
 console.log(boolean1 == boolean3);
 console.log(boolean1 === boolean3);
@@ -14,3 +15,4 @@ console.log(boolean1 === boolean3);
 //objetos
 const boolean4 = new Boolean(true);
 console.log(boolean4);
+console.log(typeof boolean4);
--- 06-Booleans/js/02-app.js
+++ 06-Booleans/js/02-app.js
@@ -1,5 +1,6 @@
 const boolean_1 = true;
 const boolean_2 = false;
 
-console.log(boolean_1 == true);
-console.log(boolean_1 == "true");
+//operador estricto
+console.log(boolean_1 === true);
+console.log(boolean_1 === "true");
