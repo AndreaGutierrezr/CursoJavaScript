22b71667b7483f3a84e59001aa2c9beb873a68a1
Andrea Gutierrez <gutierrezandrea012@gmail.com>
2023-06-28 22:56:47 -0600

    update comments to operators class

--- 05-Operadores/js/03-app.js
+++ 05-Operadores/js/03-app.js
@@ -5,5 +5,10 @@ console.log(typeof numero);
 //null
 let numero2 = null;
 console.log(numero2);
-//la espeicficacion de egma, que es quien define el estandar de lenguaje dice que los null deben ser objetos
+//la especificacion de egma, que es quien define el estandar de lenguaje dice que los null deben ser objetos
 console.log(typeof numero2);
+
+//comparando undefined y null
+console.log(numero == numero2);
+//comparador estricto
+console.log(numero === numero2);
